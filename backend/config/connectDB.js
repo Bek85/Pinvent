3260b3d1e6b1475e50b6f7e588a90a953e15a602
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-12-16 12:56:36 +0500

    setup MongoDB connection

---
+++ backend/config/connectDB.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: 'pinInvent',
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log(
+      `MongoDB connected: ${connection.connection.host}`
+      // .cyan.underline
+    );
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const connectDB = require('./config/connectDB');
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(
+      PORT,
+      console.log(
+        `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+        // .yellow.bold
+      )
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
