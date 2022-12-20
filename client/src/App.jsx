69ef87a2d38c3d14b4bb345cdabca48a35cb8774
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-12-20 16:04:40 +0500

    create auth pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from 'pinvent/pages/Home/Home';
+import Home from 'pinvent/pages/home/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Forgot from './pages/auth/Forgot';
+import Reset from './pages/auth/Reset';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgotpassword' element={<Forgot />} />
+        <Route path='/resetpassword/:resetToken' element={<Reset />} />
       </Routes>
     </Router>
   );
