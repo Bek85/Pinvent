6ef3e6df93d55c06ea5291b419ecc46c659c07d5
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-12-20 15:13:35 +0500

    create hero section in home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -24,6 +24,37 @@ export default function Home() {
           </li>
         </ul>
       </nav>
+      <section className='container hero'>
+        <div className='hero-text'>
+          <h2>Inventory & Stock Management Solution</h2>
+          <p>
+            Inventory system to control and manage proucts in the warehouse in
+            real timeand integrated to make it easier to develop your business.
+          </p>
+          <div className='hero-buttons'>
+            <button className='--btn --btn-secondary'>
+              <Link to='/dashboard'>Free Trial 1 Month</Link>
+            </button>
+          </div>
+          <div className='--flex-start'>
+            <NumberText num='14K' text='Brand Owners' />
+            <NumberText num='23K' text='Active Users' />
+            <NumberText num='500+' text='Partners' />
+          </div>
+        </div>
+        <div className='hero-image'>
+          <img src='./inv-img.png' alt='Inventory' />
+        </div>
+      </section>
     </div>
   );
 }
+
+const NumberText = ({ num, text }) => {
+  return (
+    <div className='--mr'>
+      <h3 className='--color-white'>{num}</h3>
+      <p className='--color-white'>{text}</p>
+    </div>
+  );
+};
