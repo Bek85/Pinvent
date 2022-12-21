32ebc4f82b37273c9b32bf8d0b316c9daad890ab
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-12-21 15:28:54 +0500

    create reset password page

--- client/src/pages/auth/Forgot.jsx
+++ client/src/pages/auth/Forgot.jsx
@@ -20,7 +20,7 @@ export default function Forgot() {
             </button>
             <div className={styles.links}>
               <Link to='/'> - Home</Link>
-              <Link to='/register'> - Register</Link>
+              <Link to='/login'> - Login</Link>
             </div>
           </form>
         </div>
--- client/src/pages/auth/Reset.jsx
+++ client/src/pages/auth/Reset.jsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import styles from './auth.module.scss';
+import { MdPassword } from 'react-icons/md';
+import Card from 'pinvent/components/card/Card';
+import { Link } from 'react-router-dom';
 
 export default function Reset() {
-  return <div>Reset</div>;
+  return (
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className='--flex-center'>
+            <MdPassword size={35} color='#999' />
+          </div>
+          <h2>Reset Password</h2>
+          <form>
+            <input
+              type='password'
+              placeholder='New Password'
+              required
+              name='password'
+            />
+            <input
+              type='password'
+              placeholder='Confirm New Password'
+              required
+              name='password'
+            />
+
+            <button type='submit' className='--btn --btn-primary --btn-block'>
+              Reset Password
+            </button>
+            <div className={styles.links}>
+              <Link to='/'> - Home</Link>
+              <Link to='/login'> - Login</Link>
+            </div>
+          </form>
+        </div>
+      </Card>
+    </div>
+  );
 }
