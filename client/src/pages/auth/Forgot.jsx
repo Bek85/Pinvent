3b6657d6b37a233eed9c4d21ef40f14143e03e44
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-12-21 15:21:04 +0500

    create forgot password page

--- client/src/pages/auth/Forgot.jsx
+++ client/src/pages/auth/Forgot.jsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import styles from './auth.module.scss';
+import { AiOutlineMail } from 'react-icons/ai';
+import Card from 'pinvent/components/card/Card';
+import { Link } from 'react-router-dom';
 
 export default function Forgot() {
-  return <div>Forgot</div>;
+  return (
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className='--flex-center'>
+            <AiOutlineMail size={35} color='#999' />
+          </div>
+          <h2>Forgot Password</h2>
+          <form>
+            <input type='text' placeholder='Email' required name='email' />
+
+            <button type='submit' className='--btn --btn-primary --btn-block'>
+              Get Reset Email
+            </button>
+            <div className={styles.links}>
+              <Link to='/'> - Home</Link>
+              <Link to='/register'> - Register</Link>
+            </div>
+          </form>
+        </div>
+      </Card>
+    </div>
+  );
 }
