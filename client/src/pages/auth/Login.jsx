2d6303e79a4d3a265d998d915bdb484f106ea900
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-12-21 11:54:52 +0500

    create a login page

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import styles from './auth.module.scss';
+import { BiLogIn } from 'react-icons/bi';
+import Card from 'pinvent/components/card/Card';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className='--flex-center'>
+            <BiLogIn size={35} color='#999' />
+          </div>
+          <h2>Login</h2>
+          <form>
+            <input type='text' placeholder='Email' required name='email' />
+            <input
+              type='password'
+              placeholder='Password'
+              required
+              name='password'
+            />
+            <button type='submit' className='--btn --btn-primary --btn-block'>
+              Login
+            </button>
+          </form>
+          <Link to='/forgotpassword'>Forgot Password</Link>
+          <span className={styles.register}>
+            <Link to='/'>Home</Link>
+            <p className='--px'>Don't have an account?</p>
+            <Link to='/register'>Register</Link>
+          </span>
+        </div>
+      </Card>
+    </div>
+  );
 }
