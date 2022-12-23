79f6b95e3e49e1a06e65a4b0f26ed3aab1494b9b
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-12-23 23:17:37 +0500

    add link to navigate to home page

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -4,6 +4,7 @@ import { RiProductHuntLine } from 'react-icons/ri';
 import SidebarItem from './SidebarItem';
 import { useState } from 'react';
 import menuItems from './sidebarMenuData';
+import { Link } from 'react-router-dom';
 
 export default function Sidebar({ children }) {
   const [isOpen, setIsOpen] = useState(true);
@@ -12,9 +13,13 @@ export default function Sidebar({ children }) {
     <div className='layout'>
       <div className='sidebar' style={{ width: isOpen ? '230px' : '60px' }}>
         <div className='top_section'>
-          <div className='logo' style={{ display: isOpen ? 'block' : 'none' }}>
+          <Link
+            to='/'
+            className='logo'
+            style={{ display: isOpen ? 'block' : 'none' }}
+          >
             <RiProductHuntLine size={35} style={{ cursor: 'pointer' }} />
-          </div>
+          </Link>
           <div
             className='bars'
             style={{ marginLeft: isOpen ? '100px' : '0px' }}
