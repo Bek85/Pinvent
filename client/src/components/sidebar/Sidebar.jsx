fb0f2f40585a4c32469bbf7af0eefe1bfa450eb1
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-12-23 19:48:22 +0500

    create SidebarItem component

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,39 @@
 import './Sidebar.scss';
+import { HiMenuAlt3 } from 'react-icons/hi';
+import { RiProductHuntLine } from 'react-icons/ri';
+import SidebarItem from './SidebarItem';
+import { useState } from 'react';
+import menuItems from './sidebarMenuData';
 
 export default function Sidebar({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <div className='layout'>
-      <div className='sidebar'>
-        <h2>Sidebar</h2>
+      <div className='sidebar' style={{ width: isOpen ? '230px' : '60px' }}>
+        <div className='top_section'>
+          <div className='logo' style={{ display: isOpen ? 'block' : 'none' }}>
+            <RiProductHuntLine size={35} style={{ cursor: 'pointer' }} />
+          </div>
+          <div
+            className='bars'
+            style={{ marginLeft: isOpen ? '100px' : '0px' }}
+          >
+            <HiMenuAlt3 onClick={() => setIsOpen(!isOpen)} />
+          </div>
+        </div>
+        {menuItems.map((item, index) => {
+          return <SidebarItem key={index} item={item} isOpen={isOpen} />;
+        })}
       </div>
-      <main>{children}</main>
+      <main
+        style={{
+          paddingLeft: isOpen ? '230px' : '60px',
+          transition: 'all .5s',
+        }}
+      >
+        {children}
+      </main>
     </div>
   );
 }
