b31ee6053bcd589dcd070c862b3f43948728340d
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-12-25 11:05:04 +0500

    display logged in use name in the header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 import { logoutUser } from 'pinvent/services/authService';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { SET_LOGIN, SET_NAME } from 'pinvent/redux/features/auth/authSlice';
 import { useNavigate } from 'react-router';
 
 export default function Header() {
+  const { name } = useSelector((state) => state.auth);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const logout = async () => {
@@ -17,7 +19,7 @@ export default function Header() {
       <div className='--flex-between'>
         <h3>
           <span className='--fw-thin'>Welcome, </span>
-          <span className='--color-danger'>Alex </span>
+          <span className='--color-danger'>{name} </span>
         </h3>
         <button onClick={logout} className='--btn --btn-danger'>
           Logout
