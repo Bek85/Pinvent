243b3de3f16d59667c1f6b8bb936c4e231192089
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-12-26 11:54:23 +0500

    create productService to create a new product

---
+++ client/src/redux/features/product/productService.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const PRODUCT_ENDPOINT = `${BACKEND_URL}/api/products`;
+
+//* Create a new product
+
+const createProduct = async (formData) => {
+  const res = await axios.post(`${PRODUCT_ENDPOINT}/api/products`, formData);
+
+  return res.data;
+};
+
+const productService = {
+  createProduct,
+};
+
+export default productService;
--- client/src/redux/features/product/productSlice.js
+++ client/src/redux/features/product/productSlice.js
@@ -1,4 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+import productService from './productService';
 
 const initialState = {
   product: null,
@@ -9,6 +11,25 @@ const initialState = {
   message: '',
 };
 
+// Create new product
+const createNewProduct = createAsyncThunk(
+  'products/create',
+  async (formData, thunkAPI) => {
+    try {
+      return await productService.createProduct(formData);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      console.log(message);
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 const productSlice = createSlice({
   name: 'product',
   initialState,
@@ -17,7 +38,25 @@ const productSlice = createSlice({
       console.log('store value');
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createNewProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createNewProduct.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.products.push(action.payload);
+        toast.success('Product added succesfully');
+        console.log(action.payload);
+      })
+      .addCase(createNewProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        toast.error(action.payload);
+      });
+  },
 });
 
 export const { calculateStoreVal } = productSlice.actions;
