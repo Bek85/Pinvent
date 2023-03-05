23fbee1eedde8f5323e31d570efddaabe1a3c3eb
Bekhzod Akbarov <stx.webdev@gmail.com>
2023-03-05 11:44:58 +0500

    fetch products on Dashboard page

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,10 +1,26 @@
+import ProductList from '@/components/product/product-list/ProductList';
 import useRedirectLoggedOutUser from '@/hooks/useRedirect';
+import { fetchProducts } from '@/redux/features/product/productThunk';
+import { dispatch, useSelector } from '@/redux/store';
+import { useEffect } from 'react';
 
 export default function Dashboard() {
   useRedirectLoggedOutUser('/login');
+  const { isLoggedIn } = useSelector((state) => state.auth);
+  const { products, fetchProductsStatus } = useSelector(
+    (state) => state.product
+  );
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      dispatch(fetchProducts());
+    }
+  }, [isLoggedIn, dispatch]);
+
   return (
     <div>
       <h2>Dashboard</h2>
+      <ProductList />
     </div>
   );
 }
